a4327540b65b7c7eb887c4c23d6753772d2bddd3
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-06-18 23:46:44 +0700

    commit

--- my-app/src/components/Project.jsx
+++ my-app/src/components/Project.jsx
@@ -10,15 +10,15 @@ const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
 function Project() {
     return (
         <div className=' w-full flex justify-center bg-gray-50' id='project'>
-            <div className='w-11/12 px-5 py-10 lg:w-225 flex flex-col gap-10 '>
+            <div className='w-11/12 px-5 py-10 lg:w-225 flex flex-col gap-10'>
                 <h1 className='text-3xl font-semibold md:text-4xl text-center'>Project <FontAwesomeIcon icon={faIcons} className='text-red-500 ' /></h1>
                 <div className='grid grid-cols-1  gap-8 md:grid-cols-2'>
                     {/* <Image src="/yeye.jpeg" alt='connected' width={1000} height={1000} className='overflow-hidden shadow-md rounded-2xl w-80 mx-auto' /> */}
                     <div className=''>
-                        <video width="100%" height="100%" loop={true} muted={true} autoPlay={true} alt='project' className='rounded-md shadow-md'>
+                        {/* <video width="100%" height="100%" loop={true} muted={true} autoPlay={true} alt='project' className='rounded-md shadow-md'>
                             <source src="/social-media.mov" />
-                        </video>
-                        {/* <ReactPlayer url="/social-media.mov" muted={true} loop={true} playing={true} className='' width='100%' controls={false} /> */}
+                        </video> */}
+                        <ReactPlayer url="/social-media.mov" muted={true} loop={true} playing={true} className='shadow-md rounded-md overflow-hidden' width='100%' height='100%' controls={false} />
                     </div>
                     <div className='flex flex-col gap-4 '>
                         <h4 className='text-2xl font-semibold text-red-500 '>Social media app
